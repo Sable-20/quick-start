02ae0cd07c2aa62ad60b61c682e2af5d8d15b5e2
Sable Ayala <sable262021@gmail.com>
2025-07-04 17:00:03 -0400

    testing

--- cmd/root.go
+++ cmd/root.go
@@ -1,57 +1,57 @@
-package cmd 
+package cmd
 
 import (
-  "fmt"
-  "os"
+	"fmt"
+	"os"
 
-  "github.com/spf13/cobra"
-  "github.com/spf13/viper"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var (
-  cfgFile string 
-  userLicense string 
+	cfgFile     string
+	userLicense string
 
-  rootCmd = &cobra.Command{
-    Use:    "Quick Start",
-    Short:  "Quick start is a small application I use to automate setting things up like my work environment and preferred tools.",
-  }
+	rootCmd = &cobra.Command{
+		Use:   "Quick Start",
+		Short: "Quick start is a small application I use to automate setting things up like my work environment and preferred tools.",
+	}
 )
 
 func Execute() error {
-  return rootCmd.Execute()
+	return rootCmd.Execute()
 }
 
 func init() {
-  cobra.OnInitialize(initConfig)
+	cobra.OnInitialize(initConfig)
 
-  rootCmd.PersistentFlags().StringVar(&cfgFiles, "config", "", "config file (default is $home/.cobra.toml)")
-  rootCmd.PersistentFlags().StringP("author", "a", "Sable Ayala", "Author for copyright attribution")
-  rootCmd.PersistentFlags().StringVarP(&userLicense, "license", "l", "", "name of the license for the project")
-  rootCmd.PersistentFlags().Bool("viper", true, "use viper for configuration")
-  viper.BindPFlag("author", rootCmd.PersistentFlags().Lookup("author"))
-  viper.BindPFlag("useViper", rootCmd.PersistentFlags().Lookup("viper"))
-  viper.SetDefault("author", "Sable Ayala <sable262021@gmail.com>")
-  viper.SetDefault("license", "GPLv3")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $home/.cobra.toml)")
+	rootCmd.PersistentFlags().StringP("author", "a", "Sable Ayala", "Author for copyright attribution")
+	rootCmd.PersistentFlags().StringVarP(&userLicense, "license", "l", "", "name of the license for the project")
+	rootCmd.PersistentFlags().Bool("viper", true, "use viper for configuration")
+	viper.BindPFlag("author", rootCmd.PersistentFlags().Lookup("author"))
+	viper.BindPFlag("useViper", rootCmd.PersistentFlags().Lookup("viper"))
+	viper.SetDefault("author", "Sable Ayala <sable262021@gmail.com>")
+	viper.SetDefault("license", "GPLv3")
 
-  // rootCmd.AddCommand(addCmd)
+	// rootCmd.AddCommand(addCmd)
 }
 
 func initConfig() {
-  if cfgFile != "" {
-    viper.SetConfigFile(cfgFile)
-  } else {
-    home, err := os.UserHomeDir()
-    cobra.CheckErr(err)
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
 
-    viper.AddConfigPath(home)
-    viper.SetConfigType("toml")
-    viper.SetConfigName(".cobra")
-  }
+		viper.AddConfigPath(home)
+		viper.SetConfigType("toml")
+		viper.SetConfigName(".cobra")
+	}
 
-  viper.AutomaticEnv()
+	viper.AutomaticEnv()
 
-  if err:= viper.ReadInConfig(); err == nil {
-    fmt.Println("Using config file: " viper.ConfigFileUsed())
-  }
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file: ", viper.ConfigFileUsed())
+	}
 }
