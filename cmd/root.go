1360b201e89ad1559b2452ce414ce6f10782bc16
Sable Ayala <sable262021@gmail.com>
2025-07-04 16:55:00 -0400

    init commit

---
+++ cmd/root.go
@@ -0,0 +1,57 @@
+package cmd 
+
+import (
+  "fmt"
+  "os"
+
+  "github.com/spf13/cobra"
+  "github.com/spf13/viper"
+)
+
+var (
+  cfgFile string 
+  userLicense string 
+
+  rootCmd = &cobra.Command{
+    Use:    "Quick Start",
+    Short:  "Quick start is a small application I use to automate setting things up like my work environment and preferred tools.",
+  }
+)
+
+func Execute() error {
+  return rootCmd.Execute()
+}
+
+func init() {
+  cobra.OnInitialize(initConfig)
+
+  rootCmd.PersistentFlags().StringVar(&cfgFiles, "config", "", "config file (default is $home/.cobra.toml)")
+  rootCmd.PersistentFlags().StringP("author", "a", "Sable Ayala", "Author for copyright attribution")
+  rootCmd.PersistentFlags().StringVarP(&userLicense, "license", "l", "", "name of the license for the project")
+  rootCmd.PersistentFlags().Bool("viper", true, "use viper for configuration")
+  viper.BindPFlag("author", rootCmd.PersistentFlags().Lookup("author"))
+  viper.BindPFlag("useViper", rootCmd.PersistentFlags().Lookup("viper"))
+  viper.SetDefault("author", "Sable Ayala <sable262021@gmail.com>")
+  viper.SetDefault("license", "GPLv3")
+
+  // rootCmd.AddCommand(addCmd)
+}
+
+func initConfig() {
+  if cfgFile != "" {
+    viper.SetConfigFile(cfgFile)
+  } else {
+    home, err := os.UserHomeDir()
+    cobra.CheckErr(err)
+
+    viper.AddConfigPath(home)
+    viper.SetConfigType("toml")
+    viper.SetConfigName(".cobra")
+  }
+
+  viper.AutomaticEnv()
+
+  if err:= viper.ReadInConfig(); err == nil {
+    fmt.Println("Using config file: " viper.ConfigFileUsed())
+  }
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main 
+
+import (
+  "github.com/sable-20/quick-start/cmd"
+)
+
+func main() {
+  cmd.Execute()
+}
